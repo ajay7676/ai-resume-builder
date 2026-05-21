67d65d72f283ecb1b0d5b84715079508fc584d5a
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2026-05-21 19:09:44 +0530

    Made Features & Testimonials Sections

--- frontend/src/components/pages/home/Home.jsx
+++ frontend/src/components/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
 import HeroSection from './HeroSection'
+import FeaturesSection from './FeaturesSection'
+import Testimonials from './Testimonials'
 
 const Home = () => {
   return (
     <>
       <Banner />
       <HeroSection />
+      <FeaturesSection />
+      <Testimonials />
     </>
   )
 }
---
+++ frontend/src/components/pages/home/Testimonials.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Title from './Title'
+import { LuBookUser } from 'react-icons/lu'
+import TestimonialCard from './TestimonialCard';
+
+const Testimonials = () => {
+      const cardsData = [
+        {
+            image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200',
+            name: 'Briar Martin',
+            handle: '@neilstellar',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200',
+            name: 'Avery Johnson',
+            handle: '@averywrites',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=200&auto=format&fit=crop&q=60',
+            name: 'Jordan Lee',
+            handle: '@jordantalks',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=200&auto=format&fit=crop&q=60',
+            name: 'Avery Johnson',
+            handle: '@averywrites',
+        },
+    ];
+  return (
+    <div id="features" className="flex flex-col items-center my-10 scroll-mt-12">
+       <div className="flex items-center gap-2 text-sm text-green-600 bg-green-400/10 rounded-full px-6 py-1.5">
+           <LuBookUser />
+            <span>Testimonials</span>
+        </div>
+        <Title title={`Don't just take our words` } description={`Hear what our users say about us.We are always looking for way to improve. if you have a positive experience with us,leave a review.`} />
+         <div className="marquee-row w-full mx-auto max-w-5xl overflow-hidden relative">
+                <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-white to-transparent"></div>
+                <div className="marquee-inner flex transform-gpu min-w-[200%] pt-10 pb-5">
+                    {[...cardsData, ...cardsData].map((card, index) => (
+                        <TestimonialCard key={index} card={card} />
+                    ))}
+                </div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-white to-transparent"></div>
+            </div>
+
+            <div className="marquee-row w-full mx-auto max-w-5xl overflow-hidden relative">
+                <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-white to-transparent"></div>
+                <div className="marquee-inner marquee-reverse flex transform-gpu min-w-[200%] pt-10 pb-5">
+                    {[...cardsData, ...cardsData].map((card, index) => (
+                        <TestimonialCard key={index} card={card} />
+                    ))}
+                </div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-white to-transparent"></div>
+            </div>
+    </div>
+  )
+}
+
+export default Testimonials
