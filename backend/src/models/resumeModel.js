59cea905fe00da91badfe48d5ef1c941eb051e78
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2026-06-01 21:49:08 +0530

    create resume model

---
+++ backend/src/models/resumeModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+const resumeSchema  = new mongoose.Schema(
+    {
+        title : {
+            type: String,
+            required: true,
+            trim: true,
+        },
+         userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+         },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+ module.exports = mongoose.model(
+    "Resume", resumeSchema
+ );
