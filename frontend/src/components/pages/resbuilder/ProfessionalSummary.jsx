2b220d848b25e5c04e38c766d61a58a0cecf73e0
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2026-06-05 23:58:49 +0530

    Fixed Summary Implemenation API in frontend

--- frontend/src/components/pages/resbuilder/ProfessionalSummary.jsx
+++ frontend/src/components/pages/resbuilder/ProfessionalSummary.jsx
@@ -9,8 +9,6 @@ const ProfessionalSummary = () => {
   const { resumeId } = useParams();
     const dispatch = useDispatch();
     const currentResume = useSelector((state) => state?.resume?.selectedCurrentResume);
-    console.log(currentResume._id)
-
     const [summary, setSummary] = useState("");
     useEffect(() => {
         if(currentResume?.summary){
@@ -19,15 +17,11 @@ const ProfessionalSummary = () => {
 
     } , [currentResume]);
 
-    console.log(summary)
 
     const handleSave = async () => {
            try {
              const  data =  await updatePersonalSummary(currentResume._id, {content : summary});
-              console.log(data)
-                      console.log("Summary btn was Clicked 11");
-            //  dispatch(updateCurrentResume(data.resume));
-            //  console.log(data);
+             dispatch(updateCurrentResume(data.resume));
              alert("Saved Successfully");
 
             
